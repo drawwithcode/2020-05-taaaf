19a9c8f2df543375b819fdae3571abcd9d0aeeab
taaaf <mattia.tafel@gmail.com>
2020-11-21 14:09:13 +0100

    base

---
+++ public/sketch.js
@@ -0,0 +1,38 @@
+var socket;
+
+function preload(){
+
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  background(0);
+
+  socket = io.connect("http://localhost:3000");
+  socket.on("mouse", newDrawing);
+}
+
+function newDrawing(data){
+  noStroke();
+  fill(255, 0, 100);
+  ellipse(data.x,data.y, 30);
+}
+
+function mouseDragged(){
+  console.log("Sending: " + mouseX + ", " + mouseY);
+
+  var data = {
+    x: mouseX,
+    y: mouseY
+  }
+
+  socket.emit("mouse", data);
+
+  noStroke();
+  fill(255);
+  ellipse(mouseX,mouseY,30);
+}
+
+function draw() {
+
+}
